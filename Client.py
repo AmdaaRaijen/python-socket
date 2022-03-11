78b8b57ba23e5909fa93c705e025a9043aadc7f0
AmdaaRaijen <95063290+AmdaaRaijen@users.noreply.github.com>
2022-03-11 10:53:29 +0700

    Create Client.py
    creating a new file

---
+++ Client.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def Send(msg):
+    message = msg.encode(FORMAT)
+    msg_len = len(message)
+    send_len = str(msg_len).encode(FORMAT)
+    send_len += b' ' * (HEADER - len(send_len))
+    client.send(send_len)
+    client.send(message)
+    print(client.recv(64).decode(FORMAT))
+
+Send(input('[Input] your message:'))
+Send(DISCONNECT_MESSAGE)
